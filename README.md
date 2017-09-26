79ce22f08445ac3c373dc21d70227b49a9c479d7
Kasheftin <Kasheftin@gmail.com>
2017-09-26 22:20:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bug in Routes transitions with Nuxt.js
+
+This bug based on one of the examples from the nuxt.js repo. It just wraps the page into the nuxt-child, and the transitions stop working completely.
+
+https://nuxtjs.org/examples/routes-transitions
---
+++ pages/a/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    <h1>Home page</h1>
+    <p><nuxt-link to="/a/about">About page</nuxt-link></p>
+    <p><nuxt-link to="/a/users">Lists of users</nuxt-link></p>
+  </div>
+</template>
+
+<script>
+export default {
+  transition: 'slide-left'
+}
+</script>
